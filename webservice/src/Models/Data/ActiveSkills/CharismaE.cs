02b967f805527348e03d8e66f94d523d7de976ee
NoeNze <30900057+NoeNze@users.noreply.github.com>
2017-08-10 10:41:02 -0400

    Create CharismaE.cs

---
+++ webservice/src/Models/Data/ActiveSkills/CharismaE.cs
@@ -0,0 +1,37 @@
+using FGOData.Models.Serialization;
+using System.Collections.Generic;
+
+namespace FGOData.Models.Data
+{
+    public class CharismaE : ActiveSkill
+    {
+        public CharismaB()
+        {
+            Name_EN = "Charisma E";
+            Name_JP = "カリスマ E";
+            Cooldown = 7;
+            Effects = new List<Effect> {
+                new Effect()
+                {
+                    EffectType = EffectType.Attack,
+                    Target = TargetType.Team,
+                    Duration = 3,
+                    EffectValuesType = EffectValueType.Percent,
+                    EffectValues = new List<float>
+                    {
+                        6.0f,
+                        6.6f,
+                        7.2f,
+                        7.8f,
+                        8.4f,
+                        9.0f,
+                        9.6f,
+                        10.2f,
+                        10.8f,
+                        12.0f
+                    }
+                }
+            };
+        }
+    }
+}
